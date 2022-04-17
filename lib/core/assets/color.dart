0e82c4615738ecd0162433491fb81ceb845bdccb
Daniel Partogi <daniel.partogi@stockbit.com>
2022-04-17 18:40:30 +0700

    add style

---
+++ lib/core/assets/color.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class AppColor {
+  AppColor._();
+
+  static const Color primary100 = Color(0xFFEEFBDA);
+  static const Color primary500 = Color(0xFF348721);
+  static const Color highEmphasis = Color(0xFF1A2318);
+  static const Color mediumEmphasis = Color(0xFF7B8591);
+  static const Color lowEmphasis = Color(0xFFBFC4CA);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color error500 = Color(0xFFFF4D35);
+  static const Color error300 = Color(0xFFFFA785);
+  static const Color success500 = Color(0xFF10D382);
+  static const Color info500 = Color(0xFF2E6EF7);
+  static const Color black50 = Color(0xFFF7F7F7);
+  static const Color black200 = Color(0xFFD0D0D0);
+
+}
---
+++ lib/core/assets/style.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pemanen_flutter/core/assets/color.dart';
+
+class AppStyle {
+  static TextStyle headingMedium({Color textColor = AppColor.highEmphasis}) =>
+      TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 18,
+          height: 1.4,
+          fontStyle: FontStyle.normal,
+          color: textColor);
+
+  static TextStyle headingLarge({Color textColor = AppColor.highEmphasis}) =>
+      TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 24,
+          height: 1.5,
+          fontStyle: FontStyle.normal,
+          color: textColor);
+
+  static TextStyle headline6({Color textColor = AppColor.white}) => TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 20,
+      height: 1.4,
+      color: textColor,
+      fontFamily: 'Roboto',
+      fontStyle: FontStyle.normal);
+
+  static TextStyle nominal({Color textColor = AppColor.highEmphasis}) => TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 32,
+      height: 1.25,
+      color: textColor,
+      fontStyle: FontStyle.normal);
+
+  static TextStyle label(
+          {Color textColor = AppColor.highEmphasis,
+          FontWeight weight = FontWeight.w500}) =>
+      TextStyle(
+          fontWeight: weight,
+          color: textColor,
+          fontSize: 12,
+          height: 1.4,
+          fontStyle: FontStyle.normal);
+
+  static TextStyle body(
+          {Color textColor = AppColor.highEmphasis,
+          FontWeight weight = FontWeight.normal}) =>
+      TextStyle(
+          fontWeight: weight,
+          color: textColor,
+          fontSize: 14,
+          height: 1.57,
+          fontStyle: FontStyle.normal);
+
+  static TextStyle caption(
+          {Color textColor = AppColor.highEmphasis,
+          FontWeight weight = FontWeight.w400}) =>
+      TextStyle(
+          fontWeight: weight,
+          color: textColor,
+          fontSize: 12,
+          height: 1.4,
+          fontStyle: FontStyle.normal);
+}
